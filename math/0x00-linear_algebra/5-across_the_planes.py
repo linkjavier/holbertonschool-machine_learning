da9005bef2d5c3277e40b34eeb6b8bdcd3533e7e
linkjavier <linkjavier@hotmail.com>
2020-12-06 23:46:21 -0500

    Added comments

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-"""Add 2 matricess"""
+"""Add 2 matrix"""
 
 
 def add_matrices2D(mat1, mat2):
+    """Funcxtion that add 2 matrix"""
 
     if (len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0])):
-            columns, newMatrix = [], []
-            for i in range(len(mat1)):
-                for j in range(len(mat2[0])):
-                    columns.append(mat1[i][j] + mat2[i][j])
-                newMatrix.append(columns)
-                columns = []
-            return newMatrix
+        columns, newMatrix = [], []
+        for i in range(len(mat1)):
+            for j in range(len(mat2[0])):
+                columns.append(mat1[i][j] + mat2[i][j])
+            newMatrix.append(columns)
+            columns = []
+        return newMatrix
     else:
         return (None)
 
@@ -23,4 +24,3 @@ def add_matrices2D(mat1, mat2):
 #     if np.shape(mat1) != np.shape(mat2):
 #         return None
 #     return (np.add(mat1, mat2).tolist())
-
