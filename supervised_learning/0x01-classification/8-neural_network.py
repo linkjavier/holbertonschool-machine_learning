5ea8536e2844f78421a3d56d1dd1229d863ef7ff
linkjavier <linkjavier@hotmail.com>
2021-01-06 15:49:03 -0500

    NeuralNetwork that defines a neural network with one hidden layer performing binary classification

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Python program to implement a single neuron neural network """
+import numpy as np
+
+
+class NeuralNetwork():
+    """
+        Class that defines a neural network with
+        one hidden layer performing binary classification.
+    """
+
+    def __init__(self, nx, nodes):
+        """ Init method for Neuron Class """
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
