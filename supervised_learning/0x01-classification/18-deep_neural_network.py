e65367552fcf1d662e86aa7e60094d7ef404f6d7
linkjavier <linkjavier@hotmail.com>
2021-01-07 10:53:47 -0500

    Add the public method def forward_prop(self, X):

---
+++ supervised_learning/0x01-classification/18-deep_neural_network.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+""" Python program to implement a single neuron neural network """
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """
+        class DeepNeuralNetwork that defines a deep neural network
+        performing binary classification.
+        nx : is the number of input features
+        layers : is a list representing the number of nodes
+        in each layer of the network
+        L: The number of layers in the neural network.
+        cache: A dictionary to hold all intermediary values of the network.
+        weights: A dictionary to hold all weights and biased of the network.
+
+    """
+
+    def __init__(self, nx, layers):
+
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for lay in range(self.L):
+            if not isinstance(layers[lay], int) or layers[lay] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            if lay == 0:
+                He = (np.random.randn(layers[lay], nx)
+                      * np.sqrt(2 / nx))
+                self.__weights["W{}".format(lay + 1)] = He
+            else:
+                He = (np.random.randn(layers[lay], layers[lay - 1])
+                      * np.sqrt(2 / layers[lay - 1]))
+                self.__weights["W{}".format(lay + 1)] = He
+            self.__weights["b{}".format(lay + 1)] = np.zeros((layers[lay], 1))
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
+
+    def forward_prop(self, X):
+        """
+            Add the public method def forward_prop with
+            He et al. Matrix Mult. of Weights
+            and X values (input data).
+        """
+        self.cache.update({'A0': X})
+        for i in range(self.L):
+            A = self.cache.get('A' + str(i))
+            biases = self.weights.get('b' + str(i + 1))
+            weights = self.weights.get('W' + str(i + 1))
+            Z = np.matmul(weights, A) + biases
+            self.cache.update({'A' + str(i + 1): 1 / (1 + np.exp(-Z))})
+
+        return self.cache.get('A' + str(i + 1)), self.cache
