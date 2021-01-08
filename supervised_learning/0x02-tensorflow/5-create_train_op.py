331b2e96aadcabf84a415d13569901efeedf9e56
linkjavier <linkjavier@hotmail.com>
2021-01-07 22:34:04 -0500

    Function that creates the training operation for the network

---
+++ supervised_learning/0x02-tensorflow/5-create_train_op.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Tensorflow module """
+
+import tensorflow as tf
+
+
+def create_train_op(loss, alpha):
+    """ Function that creates the training operation for the network """
+
+    train = tf.train.GradientDescentOptimizer(alpha)
+    gradient = train.compute_gradients(loss)
+    
+    return train.apply_gradients(gradient)
