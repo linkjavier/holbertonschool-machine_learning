addaebda1e9006ecbc7113b66fe9d109cc4814cf
linkjavier <linkjavier@hotmail.com>
2021-01-07 21:58:43 -0500

    Added create layer

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Tendsorflow module"""
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """ Function that creates a layer """
+
+    Initialization = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG")
+    layer = tf.layers.Dense(
+        units=n,
+        kernel_initializer=k_init,
+        activation=activation,
+        name='Layer')
+    OutputLayer = layer(prev)
+    return OutputLayer
