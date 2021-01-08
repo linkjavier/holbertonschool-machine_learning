292846535eb9ef8078f5b771c30c0e8bb88ec426
linkjavier <linkjavier@hotmail.com>
2021-01-07 21:42:43 -0500

    Testing compatibility

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """ Tensorflow Module """
 
-import tensorflow as tf
+# import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 
 def create_placeholders(nx, classes):
     """ Function that returns two placeholders,
         x and y, for the neural network
     """
-
+    tf.disable_v2_behavior()
     x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
     y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
 
