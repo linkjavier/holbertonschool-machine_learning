c4a3048e9b3f54c6baaeaa19afaf41bd2a0083c3
linkjavier <linkjavier@hotmail.com>
2021-01-07 23:58:35 -0500

    Function that evaluates the output of a neural network.

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -12,14 +12,14 @@ def evaluate(X, Y, save_path):
         saver = tf.train.import_meta_graph('{}.meta'.format(save_path))
         saver.restore(sess, '{}'.format(save_path))
 
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        y_pred = tf.get_collection('y_pred')[0]
-        loss = tf.get_collection('loss')[0]
-        accuracy = tf.get_collection('accuracy')[0]
+        x, *_ = tf.get_collection('x')
+        y, *_ = tf.get_collection('y')
+        y_pred, *_ = tf.get_collection('y_pred')
+        loss, *_ = tf.get_collection('loss')
+        accuracy, *_ = tf.get_collection('accuracy')
 
         predictionOutput = sess.run(y_pred, feed_dict={x: X, y: Y})
         lossOutput = sess.run(loss, feed_dict={x: X, y: Y})
         accuracyOutput = sess.run(accuracy, feed_dict={x: X, y: Y})
 
-    return predictionOutput, lossOutput, accuracyOutput
+    return predictionOutput, accuracyOutput, lossOutput
