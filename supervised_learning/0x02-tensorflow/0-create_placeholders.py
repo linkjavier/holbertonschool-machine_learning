5a15b4c6ecdf3229b75b2d05f1ffad6611070186
linkjavier <linkjavier@hotmail.com>
2021-01-07 21:22:32 -0500

    First Tensorflow Module

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Tensorflow Module """
+
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    """ Function that returns two placeholders,
+        x and y, for the neural network
+    """
+
+    x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
+    y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
+
+    return x, y
