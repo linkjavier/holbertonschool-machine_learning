80cc77f98ea4654796657df651627a8a33ae3373
linkjavier <linkjavier@hotmail.com>
2021-01-19 00:32:21 -0500

    Added cdf and pmf

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -14,10 +14,62 @@ class Poisson:
             else:
                 self.lambtha = float(lambtha)
         else:
-            if type(data) is not list:
+            if not isinstance(data, list):
                 raise TypeError("data must be a list")
             elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
             else:
                 lambtha = float(sum(data) / len(data))
                 self.lambtha = lambtha
+
+    def pmf(self, k):
+        """
+            Function that calculates the value of the PMF
+            for a given number of successes parameters
+        """
+        if not isinstance(k, int):
+            k = int(k)
+
+        if k < 0:
+            return 0
+
+        e = 2.7182818285
+        lambtha = self.lambtha
+        factorial = 1
+
+        for i in range(k):
+            factorial *= (i + 1)
+
+        pmf = ((lambtha ** k) * (e ** -lambtha)) / factorial
+        return pmf
+
+    def cdf(self, k):
+        """
+            Function that calculates the value of the CDF
+            for a given number of “successes”
+        """
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+
+        return cdf
+
+    def pmf(self, k):
+        """
+
+        """
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        e = 2.7182818285
+        lambtha = self.lambtha
+        factorial = 1
+        for i in range(k):
+            factorial *= (i + 1)
+        pmf = ((lambtha ** k) * (e ** -lambtha)) / factorial
+        return pmf
