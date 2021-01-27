268b3603b1d48a15c5bb4960138613865b683cb6
linkjavier <linkjavier@hotmail.com>
2021-01-27 00:09:56 -0500

    Added 0-sequential

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """Keras Neural Network Module"""
-import tensorflow as tf
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
         Function that builds a neural network with Keras
     """
-    model = tf.keras.Sequential()
-    regularizer = tf.keras.regularizers.l2(lambtha)
+    model = K.Sequential()
+    regularizer = K.regularizers.l2(lambtha)
     model.add(
-        tf.keras.layers.Dense(
+        K.layers.Dense(
             units=layers[0],
             activation=activations[0],
             kernel_regularizer=regularizer,
@@ -19,10 +19,11 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
             )))
 
     for i in range(1, len(layers)):
-        model.add(tf.keras.layers.Dropout(1 - keep_prob))
+        model.add(K.layers.Dropout(1 - keep_prob))
         model.add(
-            tf.keras.layers.Dense(
+            K.layers.Dense(
                 units=layers[i],
                 activation=activations[i],
                 kernel_regularizer=regularizer))
+
     return model
