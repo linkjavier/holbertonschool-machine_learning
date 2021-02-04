f99619375868ef597e61ffbaaf50a491f0f2b403
linkjavier <linkjavier@hotmail.com>
2021-02-04 16:17:12 -0500

    Function  that trains a model using mini-batch gradient descent

---
+++ supervised_learning/0x06-keras/4-train.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Train """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, verbose=True, shuffle=False):
+    """ Function that trains a model using
+        mini-batch gradient descent
+    """
+
+    return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs,
+                       verbose=verbose, shuffle=shuffle)
