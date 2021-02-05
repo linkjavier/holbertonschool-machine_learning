a71489bb67c2cc421527c3ce7d92cd1bf2436b6c
linkjavier <linkjavier@hotmail.com>
2021-02-05 00:25:10 -0500

    Added 7

---
+++ supervised_learning/0x06-keras/7-train.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Train"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                verbose=True, shuffle=False):
+    """Trains a model using mini-batch gradient descent
+    using early stopping
+    """
+    def scheduler(epoch):
+        """Schedule to learning rate"""
+        return alpha / (1 + decay_rate * epoch)
+
+    CallbackResult = list()
+
+    if validation_data:
+        if learning_rate_decay:
+            CallbackResult.append(K.callbacks.LearningRateScheduler(scheduler,
+                                                                    verbose=1
+                                                                    ))
+        if early_stopping:
+            CallbackResult.append(K.callbacks.EarlyStopping(monitor='val_loss',
+                                                            mode='min',
+                                                            patience=patience
+                                                            ))
+
+    return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs,
+                       verbose=verbose,
+                       callbacks=CallbackResult,
+                       validation_data=validation_data,
+                       shuffle=shuffle,
+                       )
