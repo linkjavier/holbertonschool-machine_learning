64ac028c2cf2dbd045b8c32fc7c063c57c736d29
linkjavier <linkjavier@hotmail.com>
2021-03-13 12:12:57 -0500

    Fix 8

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
-import numpy as np
+"""
+This module contains the function mat_mul.
+"""
 
 
 def mat_mul(mat1, mat2):
-    try:
-        return (np.matmul(mat1, mat2).tolist())
-    except ValueError:
+    """
+    Function to multyply 2 2D matrices.
+    """
+    rows1 = len(mat1)
+    columns1 = len(mat1[0])
+    rows2 = len(mat2)
+    columns2 = len(mat2[0])
+    if columns1 != rows2:
         return None
+    ans = []
+    for i in range(rows1):
+        current = []
+        for col in range(columns2):
+            element = 0
+            for j in range(columns1):
+                element += (mat1[i][j] * mat2[j][col])
+            current.append(element)
+        ans.append(current)
+    return ans
