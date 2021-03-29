f79e03d8066d27905af68c51a42674ab83e9f910
linkjavier <linkjavier@hotmail.com>
2021-03-28 21:38:47 -0500

    Added function tat calculates the mean and covariance of a data set

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Mean and Covariance """
+
+import numpy as np
+
+
+def mean_cov(X):
+    """ Function that calculates the mean and covariance of a data set """
+
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+
+    mean = np.zeros((1, X.shape[1]))
+    mean[0] = np.mean(X, axis=0)
+
+    covarianza = np.dot(X.T, X - mean) / (X.shape[0] - 1)
+
+    return (mean, covarianza)
