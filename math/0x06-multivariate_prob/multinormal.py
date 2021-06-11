70950e11bb3fb312c629d0cb103f1c8dc9e31d89
linkjavier <linkjavier@hotmail.com>
2021-06-11 00:49:04 -0500

    Fixed decimals

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -1,85 +1,41 @@
 #!/usr/bin/env python3
-""" Initialize """
+""" PDF Multinormal """
 
 import numpy as np
 
 
-class MultiNormal:
-    """ Class that represents a Multivariate Normal dimessageibution """
+class MultiNormal():
+    """ Class Multinormal """
 
     def __init__(self, data):
-        """ Instance contructor"""
+        """ Constructor Init  """
 
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
-            raise TypeError("data must be a 2D numpy.ndarray")
-
+            raise TypeError('data must be a 2D numpy.ndarray')
         if data.shape[1] < 2:
-            raise ValueError("data must contain multiple data points")
+            raise ValueError('data must contain multiple data points')
 
-        mean = np.mean(data, axis=1).reshape((data.shape[0], 1))
-
-        self.mean = mean
-        self.cov = np.matmul(data - self.mean, data.T) / (data.shape[1] - 1)
+        _, n = data.shape
+        self.mean = np.mean(data, axis=1, keepdims=True)
+        X_mean = data - self.mean
+        self.cov = np.dot(X_mean, X_mean.T) / (n - 1)
 
     def pdf(self, x):
-        """ Public instance method def that calculates
-            the PDF at a data point """
+        """ Method that calculates the PDF at a data point  """
 
         if not isinstance(x, np.ndarray):
-            raise TypeError("x must be a numpy.ndarray")
+            raise TypeError('x must be a numpy.ndarray')
 
-        dimensions = self.mean.shape[0]
+        d, _ = self.cov.shape
 
-        if len(x.shape) != 2:
-            text = 'x must have the shape ({}, 1)'.format(dimensions)
-            raise ValueError(text)
+        if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
+            raise ValueError('x must have the shape ({}, 1)'.format(d))
 
-        if x.shape[0] != dimensions or x.shape[1] != 1:
-            text = 'x must have the shape ({}, 1)'.format(dimensions)
-            raise ValueError(text)
+        determinant = np.linalg.det(self.cov)
+        base = 1 / (((2 * np.pi) ** (d / 2)) * (np.sqrt(determinant)))
+        factor = np.dot((x - self.mean).T, np.linalg.inv(self.cov))
+        factor = np.dot(factor, (x - self.mean))
+        pdf = base * np.exp((-1 / 2) * factor)
+        pdf = pdf[0][0]
 
-        base = 1 / np.sqrt(((2 * np.pi) ** dimensions)
-                           * (np.linalg.det(self.cov)))
-
-        factor = np.matmul(-(x - self.mean).T, np.linalg.inv(self.cov))
-
-        multiplier = (x - self.mean) / 2
-
-        product = np.matmul(factor, multiplier)
-
-        exponent = np.exp(product)
-        pdf = base * exponent
-
-        return (pdf.reshape(-1)[0])
-
-    # def pdf(self, x):
-    #     """ Public instance method def that calculates
-    #         the PDF at a data point """
-
-    #     if not isinstance(x, np.ndarray):
-    #         raise TypeError("x must be a numpy.ndarray")
-
-    #     d = self.cov.shape[0]
-
-    #     if len(x.shape) != 2:
-    #         raise ValueError('x must have the shape ({}, 1)'.format(d))
-
-    #     if x.shape[1] != 1 or x.shape[0] != d:
-    #         raise ValueError('x must have the shape ({}, 1)'.format(d))
-
-    #     dimensions = x.shape[0]
-
-    #     mean = self.mean
-    #     cov = self.cov
-
-    #     covarianceDeterminant = np.linalg.det(cov)
-    #     covarianceInverse = np.linalg.inv(cov)
-
-    #     denominator = np.sqrt(((2 * np.pi) ** dimensions)
-    #                           * covarianceDeterminant)
-    #     exponent = -0.5 * \
-    #         np.matmul(np.matmul((x - mean).T, covarianceInverse), x - mean)
-
-    #     pdf = (1 / denominator) * np.exp(exponent[0][0])
-
-    #     return pdf
+        return pdf
