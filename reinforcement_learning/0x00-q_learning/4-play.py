6c8fe6e616cefa4dfd3df8fca73621fd54bc1a67
linkjavier <linkjavier@hotmail.com>
2021-06-23 00:04:24 -0500

    Function that has the trained agent play an episode

---
+++ reinforcement_learning/0x00-q_learning/4-play.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Play -QLearning """
+import numpy as np
+epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
+load_frozen_lake = __import__('0-load_env').load_frozen_lake
+q_init = __import__('1-q_init').q_init
+train = __import__('3-q_learning').train
+
+
+def play(env, Q, max_steps=100):
+    """ Function that has the trained agent play an episode """
+
+    state = env.reset()
+    done = False
+    env.render()
+
+    for step in range(max_steps):
+        action = np.argmax(Q[state, :])
+        new_state, TotalRewards, done, info = env.step(action)
+        env.render()
+
+        if done is True:
+            if TotalRewards == 1:
+                print(TotalRewards
+                      )
+                break
+        state = new_state
+
+    env.close()
+
+    return TotalRewards
