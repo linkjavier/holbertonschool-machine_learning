360329841bcd8c3d83d572923e4dc309bee31345
linkjavier <linkjavier@hotmail.com>
2021-08-02 00:37:34 -0500

    Added 3

---
+++ pipeline/0x02-databases/3-insert_value.sql
@@ -0,0 +1,4 @@
+-- Script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+-- cat 3-insert_value.sql | sudo mysql -hlocalhost -uroot -p db_0
+
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
