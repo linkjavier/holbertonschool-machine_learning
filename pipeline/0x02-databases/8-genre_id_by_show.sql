e1da25a5b9d551061fff1cc1ec4a23262b100383
linkjavier <linkjavier@hotmail.com>
2021-08-02 23:34:31 -0500

    Added 7 to 11

---
+++ pipeline/0x02-databases/8-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- cat 8-genre_id_by_show.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvs
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
